d9658ee10e3653a8c040d46735d8f0a5a0e56c81
NowWeAre-Free <kontodarm@wp.pl>
2023-07-08 13:17:58 +0200

    add cleanArray function

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-const advices = ["walcz", "przemyśl to jeszcze raz"];
+let advices = ["walcz", "przemyśl to jeszcze raz"];
 
 const addButton = document.querySelector(".add");
 const cleanButton = document.querySelector(".clean");
 const showAdivce = document.querySelector(".showAdvice");
 const showOptions = document.querySelector(".showOptions");
+
 const input = document.querySelector("input");
 
-const add = (e) => {
+const addToArray = (e) => {
   e.preventDefault();
   if (input.value.length) {
     advices.push(input.value);
     alert(`dodano ${input.value}`);
   }
+  input.value = "";
 };
 
-addButton.addEventListener("click", add);
+const cleanArray = (e) => {
+  e.preventDefault();
+  advices = ["walcz", "przemyśl to jeszcze raz"];
+  alert("zresetowano możliwości!");
+};
+
+addButton.addEventListener("click", addToArray);
+cleanButton.addEventListener("click", cleanArray);
+showAdivce.addEventListener("click", showAdvice);
