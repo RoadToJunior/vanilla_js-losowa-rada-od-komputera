9011b885d195c26a99f0ce09ff8172e637dce11a
NowWeAre-Free <kontodarm@wp.pl>
2023-07-08 12:33:13 +0200

    add all buttons to vars

---
+++ main.js
@@ -0,0 +1,6 @@
+const advices = ["walcz", "przemyśl to jeszcze raz"];
+
+const addButton = document.querySelector(".add");
+const cleanButton = document.querySelector(".clean");
+const showAdivce = document.querySelector(".showAdvice");
+const showOptions = document.querySelector(".showOptions");
