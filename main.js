258fa93e6232620a5ef16d8c2931c72aa4ede9ca
NowWeAre-Free <kontodarm@wp.pl>
2023-07-08 13:09:42 +0200

    add function to adding button

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const addButton = document.querySelector(".add");
 const cleanButton = document.querySelector(".clean");
 const showAdivce = document.querySelector(".showAdvice");
 const showOptions = document.querySelector(".showOptions");
+const input = document.querySelector("input");
+
+const add = (e) => {
+  e.preventDefault();
+  if (input.value.length) {
+    advices.push(input.value);
+    alert(`dodano ${input.value}`);
+  }
+};
+
+addButton.addEventListener("click", add);
